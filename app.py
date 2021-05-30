301ee6c3ab5fc8c2225c7d982e99335483fdcceb
anjali7786 <guptaanj7786@gmail.com>
2021-05-30 19:12:59 +0530

    web page added

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template , request , jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('./index.html')
+
+
+app.run(debug = True)
